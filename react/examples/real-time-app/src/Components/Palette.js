10e481c4cc0b21c6ec89393147e2505271ee36f3
yigittopm <yigittopm@hotmail.com>
2022-01-16 10:50:11 +0300

    palette added

---
+++ react/examples/real-time-app/src/Components/Palette.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Palette() {
+    return (
+        <div>
+            <input type="color"/>
+            <button>Select</button>
+        </div>
+    )
+}
+
+export default Palette
